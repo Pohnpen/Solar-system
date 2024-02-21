f0e423c025a587f325deca97e5eef69e454eee68
Pohnpen Daennanart <pohnpen.a@kkumail.com>
2024-02-21 22:18:59 +0700

    orbital position and move method

--- lib/vector.py
+++ lib/vector.py
@@ -1,5 +1,7 @@
 # Vector
 
+from math import cos, sin, pi
+
 class Vector():
 
     def __init__(self, x, y):
@@ -18,6 +20,9 @@ class Vector():
         x, y = self.x * self.scalar, self.y * self.scalar
         return Vector(x, y)
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
     def distance(self, other):
         # distance return as a float
         distance = ((abs(self.x - other.x))**2 + (abs(self.y - other.y))**2)**0.5
@@ -27,6 +32,10 @@ class Vector():
         # displacement return as an intiger
         displacement = abs(self.x - other.x), abs(self.y - other.y) # (2.2, 4.3)
         return Vector(displacement[0], displacement[1])
+
+    def angular_displacement(self, distance, radians):
+        radians = radians % (2*pi)
+        return Vector(x=self.x+cos(radians)*distance, y=self.y+sin(radians)*distance)
 
 if __name__ == "__main__":
     a = Vector(0, 0)
