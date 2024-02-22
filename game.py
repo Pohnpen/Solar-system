4848bb9cb581ee41140e35c9c1bb14a8c6922544
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-22 11:51:02 +0700

    refactor

--- game.py
+++ game.py
@@ -15,7 +15,8 @@ pygame.display.set_caption("Solar System Viewer")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 Yellow = (225, 225, 0)
-
+# Define constants
+# Draw a circle in the center of the window
 center = (window_size[0] // 2, window_size[1] // 2)
 
 # Main loop
@@ -28,8 +29,6 @@ while running:
     # Fill the background
     screen.fill(BLACK)
 
-    # Draw a circle in the center of the window
-    # center = (window_size[0] // 2, window_size[1] // 2)  # Calculate the center of the window
     # pygame.draw.circle(screen, WHITE, center, 50)  # Draw circle with radius 50
     pygame.draw.circle(screen, Yellow, center, 50)
 
